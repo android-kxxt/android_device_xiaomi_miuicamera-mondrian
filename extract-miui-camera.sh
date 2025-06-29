7cdf95765b26a8da05cb22bc30cc762db6faa906
kxxt <rsworktech@outlook.com>
2025-06-29 19:01:55 +0800

    Update to mondrian OS2.0.9.0.VMNCNXM
    Change-Id: Ie623b8ba5247cc29ab7f2f6f4fb5e5791a1cdeb6

--- extract-miui-camera.sh
+++ extract-miui-camera.sh
@@ -3,8 +3,8 @@
 ../../../prebuilts/jdk/jdk21/linux-x86/bin/java \
     -jar ../../../prebuilts/extract-tools/common/apktool/apktool.jar \
     d ../../../vendor/xiaomi/miuicamera-mondrian/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk \
-    -o MiuiCameraExtracted -f
-cd MiuiCameraExtracted
+    -o MiuiCamera -f
+cd MiuiCamera
 git init
 git add .
 git commit -m "Initial Commit"
